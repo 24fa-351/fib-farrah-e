7ba488fbe21737bf28b0f43b1486cd42561a2b54
Ermeje <farrah.e@csu.fullerton.edu>
2024-09-07 08:23:06 -0700

    trying

--- fib.c
+++ fib.c
@@ -1,6 +1,62 @@
 #include <stdio.h>
 
+int numInputted, ir, fibN, num;
+int printRecursion(int n){
+   if (n == 0){
+      fibN= 0;
+   }
+   if (n == 1){
+      fibN = 1;
+   }
+   else{
+      fibN = (printRecursion(n-1)+printRecursion(n-2));
+   }
+   printf("The Fibonnacci number is: %d", fibN);
+}
+
+void printIteration(int n){
+   int term1=1, term2=0;
+   for (int i = 1; i <=n; i++){
+      if (i==1) {
+         fibN = 0;
+      }
+      if (i==2) {
+         fibN = 1;
+      }
+      if(i > 2){
+         fibN = term1 + term2;
+         term2=term1;
+         term1=fibN;
+      }
+   }
+   printf("The Fibonnacci number is: %d", fibN);
+
+}
+
+void recursionOrIteration(char t){
+   if(t=='i'){
+      printIteration(num);
+   } 
+   else{
+      printRecursion(num);
+   }
+}
+
+void gatheringNumber(int inputted){
+   int numFromFile;
+   FILE *gatheringFiles = fopen("fileNumber.txt", "r");
+   fscanf(gatheringFiles, "%d", &numFromFile);
+   num = numFromFile + inputted;
+   fclose(gatheringFiles);
+}
+
 int main() {
    printf("Hello!\n");
+   printf("Please input 'r' for recursion) or 'i' for iteration: ");
+   ir = getchar();
+   printf("Please input an integer: ");
+   scanf("%d", &numInputted);
+   gatheringNumber(numInputted);
+   recursionOrIteration(ir); 
    return 0;
 }
